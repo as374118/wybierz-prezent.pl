c61cb0d7dd8fd5a3cd0a3ccf27a975aab85ce1af
alexAlice <alex@alice.si>
2018-12-26 00:56:12 +0100

    first version of wybierz-prezent.pl

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-// TODO
+$(document).ready(function() {
+
+  // Open menu
+  $('.navbar-toggle').click(function() {
+    $('.nav').toggleClass('open');
+    $('body').toggleClass('fixed');
+    $(this).toggleClass('active');
+  });
+
+  // Desire add class
+  $('.desire-list li').click(function() {
+    $('.desire-list li').removeClass('current');
+    $(this).addClass('current');
+  });
+
+});
