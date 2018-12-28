4206a51bb626b82cb6d966fd252eb1445d94f520
alexAlice <alex@alice.si>
2018-12-28 03:39:14 +0100

    privacy policy added

--- assets/translations/en.js
+++ assets/translations/en.js
@@ -6,6 +6,9 @@ export default
     "FAQ": "FAQ",
     "LogIn": "Log in",
 
+    "ReservedRights": "All right reserved",
+    "PrivacyPolicy": "Privacy policy",
+
     "Statistics": "Statistics",
     "WeKnowWhatIsTheBestGift": "We know what is the best gift!",
     "PortalDescription": "We are nonprofit portal which contains information about statistics of the most popular and desired gifts.",
--- assets/translations/pl.js
+++ assets/translations/pl.js
@@ -6,6 +6,8 @@ export default
     "FAQ": "Pytania",
     "LogIn": "Zaloguj się",
 
+    "ReservedRights": "Wszystkie prawa zastrzeżone",
+    "PrivacyPolicy": "Polityka prywatności",
 
     "WeKnowWhatIsTheBestGift": "Wiemy jaki prezent jest najlepszy!",
     "PortalDescription": "Jesteśmy niekomercyjnym portalem zawierającym statystki najpopularniejszych oraz najbardziej poządanych prezentów.",
