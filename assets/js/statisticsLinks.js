fa595760ed328db60acee4971eb18b4323f8a109
alexAlice <alex@alice.si>
2018-12-31 18:30:38 +0300

    added statistics links

---
+++ assets/js/statisticsLinks.js
@@ -0,0 +1,29 @@
+const links = [
+    {
+        img: 'assets/img/icons/008-student.png',
+        text: 'Dla taty',
+        link: 'statisctics.html?filter=father'
+    },
+    {
+        img: 'assets/img/icons/002-boy.png',
+        text: 'Dla chłopaka',
+        link: 'statisctics.html?filter=boy'
+    },
+    {
+        img: 'assets/img/icons/003-girl.png',
+        text: 'Dla dziewczyny',
+        link: 'statisctics.html?filter=girl'
+    },
+    {
+        img: 'assets/img/icons/004-man.png',
+        text: 'Dla faceta',
+        link: 'statisctics.html?filter=man'
+    },
+    {
+        img: 'assets/img/icons/013-mother.png',
+        text: 'Uniwersalne',
+        link: 'statisctics.html?filter=man'
+    },
+];
+
+export default links;
--- assets/js/vueBodyApp.js
+++ assets/js/vueBodyApp.js
@@ -1,10 +1,12 @@
 import lang from './lang.js';
+import statisticsLinks from './statisticsLinks.js';
 
 let app = new Vue({
     el: '#main-container',
     data: {
         showDetailPortalDescription: false,
         lang: lang.getLang() || lang.defaultLang,
+        statisticsLinks: statisticsLinks
     },
     methods: {
         getLang: lang.getLang,
